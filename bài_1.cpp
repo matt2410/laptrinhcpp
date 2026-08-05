7d9b562f553e5da6f407ec4f6edbdbfb5aaa3321
Louis <nhatquang241008@gmail.com>
2026-08-05 22:10:50 +0700

    nộp bài tập bài 1

---
+++ bài_1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string name;
+    cout << "Nhap ten cua ban: " << endl;
+    cin >> name;
+    cout << "Xin chao, " << name << "!" << endl;
+    return 0;
+}
