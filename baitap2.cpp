4727bc3e930d8f15e4fea3fb86bef196247eb5ab
Louis <nhatquang241008@gmail.com>
2026-08-06 09:20:41 +0700

    Add baitap2: nhap va in thong tin

---
+++ baitap2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string name;    // Declare a string variable to store the user's name
+    string className;
+
+    cout << "Nhập họ và tên của bạn: ";
+    getline(cin, name);
+
+    cout << "Nhập lớp của bạn: ";
+    getline(cin, className);
+
+    cout << "Xin chào, " << name << ", " << "lớp " << className << endl;
+    return 0;
+}
