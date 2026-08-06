4efc442d0d298aa8d7c08f5cf1b2472a2a0fe7f0
Louis <nhatquang241008@gmail.com>
2026-08-06 09:20:24 +0700

    Add baitap1: in thong tin ca nhan

---
+++ baitap1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Họ và tên: Nguyễn Chu Nhật Quang" << endl;
+    cout << "Lop: C++ K01" << endl;
+    cout << "Ngày sinh: 24/10/2008" << endl;
+    cout << "Sở thích: nghe nhạc, sưu tầm mô hình" << endl;
+    cout << "Mục tiêu: trở thành lập trình viên chuyên nghiệp" << endl;
+    return 0;
+}
