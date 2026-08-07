f099fe31d939e4d33a7beb71de49c4b40aef3310
Louis <nhatquang241008@gmail.com>
2026-08-07 20:21:03 +0700

    sửa đổi lần 1

--- baitap2.cpp
+++ baitap2.cpp
@@ -3,8 +3,7 @@ using namespace std;
 
 int main()
 {
-    string name;    // Declare a string variable to store the user's name
-    string className;
+    string name, className; // Declare a string variable to store the user's name
 
     cout << "Nhập họ và tên của bạn: ";
     getline(cin, name);
